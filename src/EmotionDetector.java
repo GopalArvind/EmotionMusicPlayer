bf647fc474a7f6502a8d0124a781565e921d99b8
Suigetsu <present.akash@gmail.com>
2015-03-20 11:10:58 +0530

    Added temporary GUI for the application

--- src/EmotionDetector.java
+++ src/EmotionDetector.java
@@ -4,11 +4,12 @@ import org.opencv.core.Core;
 
 
 public class EmotionDetector {
-	private static String mode = "Testing";
+	public static String mode = "Testing";
 	private String emotionOfFace;	//Will be set after calculation of eigen vectors and comparisons.
 	
-	public String imagePath;	//User input
-	public String emotion;		//User input
+	public static String imagePath;	//User input
+	public static String emotion;		//User input
+	GuiComponent gui = new GuiComponent();	//Remove in next phase.
 	
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -21,7 +22,9 @@ public class EmotionDetector {
 	
 	public void start() {
 		getInput();
+		imagePath = FeatureExtractor.testImagesLoc + imagePath;
 		FeatureExtractor featureExtractor = new FeatureExtractor();
+		System.out.println(imagePath);
 		featureExtractor.start(imagePath);
 		Covariance covariance = new Covariance();
 		
@@ -33,11 +36,22 @@ public class EmotionDetector {
 //			covariance.getMeanSubtractedImage(imagePath, "");
 			System.out.println("The emotion is: " + emotionOfFace);
 		}
+		
+		if(!mode.equals("Training")) {
+			gui.showResult(emotionOfFace);
+		}
 	}
 	
 	public void getInput() {
 		//Set user inputs.
 		imagePath = FeatureExtractor.testImagesLoc + "WP_20150124_13_35_53_Pro.jpg";
 		emotion = "Sad";
+	
+		try {
+			synchronized(gui) {
+				gui.showGui();
+				gui.wait();
+			}
+		} catch(Exception e) {e.printStackTrace();}
 	}
 }
